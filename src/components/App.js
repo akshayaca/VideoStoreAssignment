9c303a2d8a218ef6737b32827c50a612c1f045d0
akshayaca <akshayaca@outlook.com>
2024-03-06 18:21:46 -0500

    Completed base with hero

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import Header from './Header'; 
+import Header from './Header';
+import BaseComponent from './BaseComponent'; 
 import Footer from './Footer';
 import '../css/App.css';
 
 const App = () => (
-  <div className="App">
-  <div className='min-h-screen'>
-      <Header />
-      <Footer />
-      
-  </div>
-  </div>
+  <div className="App min-h-screen flex flex-col">
+  <Header />
+  <main className="bg-black flex-grow">
+  <BaseComponent />
+  </main>
+  <Footer />
+</div>
 );
+
 
 export default App;
---
+++ src/components/BaseComponent.js
@@ -0,0 +1,15 @@
+// BaseComponent.js
+
+import React from 'react';
+import Hero from './Hero'; 
+
+const BaseComponent = () => {
+  return (
+    <div className="bg-custom-dark"> 
+     
+     <Hero/>
+    </div>
+  );
+};
+
+export default BaseComponent;
