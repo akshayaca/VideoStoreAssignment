5b9d327e210adf9de31e151c6649bfeeed875b4c
akshayaca <akshayaca@outlook.com>
2024-03-06 23:54:18 -0500

    added description data to cards

--- src/components/FeaturedMoviesAndTv.js
+++ src/components/FeaturedMoviesAndTv.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react';
 const FeaturedMoviesAndTVShows = () => {
     const [featuredMovies, setFeaturedMovies] = useState([]);
     const [featuredTVShows, setFeaturedTVShows] = useState([]);
+    const [selectedMovieId, setSelectedMovieId] = useState(null);
+    const [selectedShowId, setSelectedShowId] = useState(null);
     const [error, setError] = useState(null);
 
     useEffect(() => {
@@ -24,26 +26,46 @@ const FeaturedMoviesAndTVShows = () => {
         return <div>Error: {error.message}</div>;
     }
 
+    const handleMovieClick = (id) => {
+        setSelectedMovieId(selectedMovieId === id ? null : id);
+    };
+
+    const handleShowClick = (id) => {
+        setSelectedShowId(selectedShowId === id ? null : id);
+    };
+
     return (
         <div className="bg-black p-6">
             <h2 className="text-white text-2xl mb-4">Featured Movies</h2>
             <div className="grid grid-flow-col auto-cols-max gap-4 overflow-x-auto hide-scrollbar">
-            {featuredMovies.map(movie => (
-                <div key={movie.id} className="bg-black border border-black mx-1 my-1 flex flex-col items-center shadow-neon-blue card" style={{ minWidth: '250px', maxWidth: '250px' }}>
-                    <img src={movie.poster} alt={movie.title} className="w-full h-full object-cover" />
-                    <p className="text-white font-semibold text-center p-2">{movie.title}</p>
-                </div>
-            ))}
+                {featuredMovies.map(movie => (
+                    <div key={movie.id} className="bg-black border border-black mx-1 my-1 flex flex-col items-center shadow-neon-blue cursor-pointer card " style={{ minWidth: '250px', maxWidth: '250px' }} onClick={() => handleMovieClick(movie.id)}>
+                        {selectedMovieId === movie.id ? (
+                            <p className="text-white p-2">{movie.description}</p>
+                        ) : (
+                            <>
+                                <img src={movie.poster} alt={movie.title} className="w-full h-full" />
+                                <p className="text-white font-semibold text-center p-2">{movie.title}</p>
+                            </>
+                        )}
+                    </div>
+                ))}
             </div>
 
             <h2 className="text-white text-2xl mt-8 mb-4">Featured TV Shows</h2>
             <div className="grid grid-flow-col auto-cols-max gap-4 overflow-x-auto hide-scrollbar">
-            {featuredTVShows.map(show => (
-                <div key={show.id} className="bg-black border border-black mx-1 my-1 flex flex-col items-center shadow-neon-blue card " style={{ minWidth: '250px', maxWidth: '250px' }}>
-                    <img src={show.poster} alt={show.title} className="w-full h-full object-cover" />
-                    <p className="text-white font-semibold text-center p-2">{show.title}</p>
-                </div>
-            ))}
+                {featuredTVShows.map(show => (
+                    <div key={show.id} className="bg-black border border-black mx-1 my-1 flex flex-col items-center shadow-neon-blue cursor-pointer card " style={{ minWidth: '250px', maxWidth: '250px' }} onClick={() => handleShowClick(show.id)}>
+                        {selectedShowId === show.id ? (
+                            <p className="text-white p-2">{show.description}</p> 
+                        ) : (
+                            <>
+                                <img src={show.poster} alt={show.title} className="w-full h-full object-cover" />
+                                <p className="text-white font-semibold text-center p-2">{show.title}</p>
+                            </>
+                        )}
+                    </div>
+                ))}
             </div>
         </div>
     );
