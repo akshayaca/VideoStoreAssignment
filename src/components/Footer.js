b189ead9457247049f8dda76a0f395044155a811
akshayaca <akshayaca@outlook.com>
2024-03-08 12:01:24 -0500

    minor chnages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,10 @@ const Footer = () => {
       <div className="max-w-screen-lg mx-auto">
         <p className="text-xl mb-4">© 2024 Your Company. All rights reserved.</p>
         <div className="flex flex-wrap justify-center gap-4">
-          <a href="#terms" className="hover:text-pink-500 text-lg">Terms of Use</a>
-          <a href="#policy" className="hover:text-pink-500 text-lg">Privacy Policy</a>
-          <a href="#preference" className="hover:text-pink-500 text-lg">Cookie Preferences</a>
-          <a href="#info" className="hover:text-pink-500 text-lg">Corporate Information</a>
+          <a href="#terms" className="hover:text-blue-500 text-lg">Terms of Use</a>
+          <a href="#policy" className="hover:text-blue-500 text-lg">Privacy Policy</a>
+          <a href="#preference" className="hover:text-blue-500 text-lg">Cookie Preferences</a>
+          <a href="#info" className="hover:text-blue-500 text-lg">Corporate Information</a>
         </div>
       </div>
     </footer>
