4e9768a96f893aa075149c7a11faf15858b84cd0
akshayaca <akshayaca@outlook.com>
2024-03-08 12:25:27 -0500

    minor chnages

--- src/components/BaseComponent.js
+++ src/components/BaseComponent.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import Hero from './Hero'; 
 import FeaturedMoviesAndTVShows from './FeaturedMoviesAndTv';
-import ContentSection from './ContentSection'; // Import the ContentSection component
+import ContentSection from './ContentSection'; 
 
 
 const BaseComponent = () => {
