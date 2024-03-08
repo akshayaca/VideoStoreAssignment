f17fda17a68b0349bfc5aaba7a67945b86d5d40c
akshayaca <akshayaca@outlook.com>
2024-03-08 11:58:06 -0500

    minor chnages

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import { FaUserAlt, FaBars, FaHome } from 'react-icons/fa';
 import { IoMdFilm } from 'react-icons/io';
 import { MdLiveTv } from 'react-icons/md';
-import LoginModal from './LoginModal'; // Import your login modal component here
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import LoginModal from './LoginModal'; 
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
@@ -20,24 +20,24 @@ const Header = () => {
   return (
     <header className="bg-black text-white flex justify-between items-center p-4 sticky top-0 z-50">
       <div className="flex items-center">
-        <FaBars className="text-xl  cursor-pointer mr-4 md:hidden" onClick={() => setIsNavOpen(!isNavOpen)} />
+        <FaBars className="text-2xl  cursor-pointer mr-4 md:hidden" onClick={() => setIsNavOpen(!isNavOpen)} />
         <nav className={`${isNavOpen ? 'flex' : 'hidden'} absolute flex-col bg-black w-full p-4 top-full left-0 md:static md:flex md:flex-row md:items-center md:w-auto md:p-0 md:top-auto md:left-auto md:bg-transparent overflow-auto max-h-60 md:max-h-full`}>
           <Link to="/moviestvlisting" className="flex items-center gap-2 hover:text-blue-500 mb-2 md:mb-0 md:mr-4">
-            <IoMdFilm className="text-xl" />
-            <span className="font-bold">Movies</span>
+            <IoMdFilm className="text-2xl" />
+            <span className="font-bold text-2xl">Movies</span>
           </Link>
           <Link to="/moviestvlisting" className="flex items-center gap-2 hover:text-blue-500 mb-2 md:mb-0 md:mr-4">
-            <MdLiveTv className="text-xl" />
-            <span className="font-bold">TV Shows</span>
+            <MdLiveTv className="text-2xl" />
+            <span className="font-bold text-2xl">TV Shows</span>
           </Link>
         </nav>
       </div>
       <div className="flex items-center">
-        <Link to="/" className="text-xl hover:text-blue-500 ml-4 cursor-pointer">
+        <Link to="/" className="text-2xl hover:text-blue-500 ml-4 cursor-pointer">
           <FaHome />
         </Link>
-        <FaUserAlt className="text-xl hover:text-blue-500 ml-4 cursor-pointer" onClick={openModal} />
-        {isModalOpen && <LoginModal closeModal={closeModal} />}
+        <FaUserAlt className="text-2xl hover:text-blue-500 ml-4 cursor-pointer" onClick={openModal} />
+        {isModalOpen && <LoginModal closeModal={closeModal} />} 
       </div>
     </header>
   );
