bc53f54d05232aac206ab8efdea2e23800f7324c
akshayaca <akshayaca@outlook.com>
2024-03-11 19:33:26 -0400

    df

--- src/server.js
+++ src/server.js
@@ -1,36 +1,19 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs'); // Require the file system module
+const jsonServer = require('json-server');
 const app = express();
+const port = process.env.PORT || 3001;
 
-// Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Read data from db.json
-const dbData = require('./db.json');
 
-// Endpoint to serve featured movies JSON data
-app.get('/featuredMovies', (req, res) => {
-  res.json(dbData.featuredMovies);
-});
+const router = jsonServer.router('db.json'); 
+app.use('/api', router); 
 
-// Endpoint to serve featured TV shows JSON data
-app.get('/featuredTVShows', (req, res) => {
-  res.json(dbData.featuredTVShows);
-});
-
-// Endpoint to serve collection JSON data
-app.get('/collection', (req, res) => {
-  res.json(dbData.collection);
-});
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
